8f5407e6ab441a01d75196dd48a410ac7d3a2980
Galina <gal-dej@yandex.ru>
2022-10-03 16:34:18 +0300

    Task complite

--- Task02/Program.cs
+++ Task02/Program.cs
@@ -3,3 +3,17 @@
 // a = 5; b = 7 -> max = 7
 // a = 2 b = 10 -> max = 10
 // a = -9 b = -3 -> max = -3
+Console.WriteLine("Enter a number");
+string firstnumber = Console.ReadLine ();
+int firstNumber = int.Parse(firstnumber);
+Console.WriteLine("Enter next number");
+string secondnumber = Console.ReadLine ();
+int secondNumber = int.Parse(secondnumber);
+if(firstNumber>secondNumber)
+{
+    Console.WriteLine($"a = {firstNumber}; b = {secondNumber} -> max = {firstNumber}");
+} 
+else
+{
+    Console.WriteLine($"a = {firstNumber}; b = {secondNumber} -> max = {secondNumber}");
+}
