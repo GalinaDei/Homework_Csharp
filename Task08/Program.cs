05966b18081fd4b29638f7b8331fd1643af1ffc6
Galina <gal-dej@yandex.ru>
2022-10-04 11:57:50 +0300

    Tsak 08 have done

--- Task08/Program.cs
+++ Task08/Program.cs
@@ -2,3 +2,14 @@
 // а на выходе показывает все чётные числа от 1 до N.
 // 5 -> 2, 4
 // 8 -> 2, 4, 6, 8
+Console.WriteLine("Enter a number");
+int N = int.Parse(Console.ReadLine()??"");
+Console.Write($"{N} -> ");
+for (int count=1; count<N; count++)
+{
+ if (count%2 == 0)
+ {
+    Console.Write($"{count}, ");
+ }
+}
+
